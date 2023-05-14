7623c52b8b063937bb90a6d91644c194d983c5cf
Med Taha Hamdi <hamdi.taha@esprit.tn>
2023-05-14 06:27:29 +0200

    a
    a

---
+++ src/context/index.jsx
@@ -0,0 +1,107 @@
+import React, { useContext, createContext } from 'react';
+
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+  const { contract } = useContract("0x0df3Ee423d8CCDDA497c9a443A47A818Fb164C42");
+  const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+
+  const address = useAddress();
+  const connect = useMetamask();
+
+  const publishCampaign = async (form) => {
+    console.log("Form:", form);
+    const targetValue = ethers.utils.parseUnits(String(form.target), 18).toString();
+    console.log("Target value:", targetValue);
+  
+    try {
+      const data = await createCampaign([
+        form.name,
+        form.title,
+        form.description,
+        targetValue,
+        new Date(form.deadline).getTime(),
+        form.image
+      ]);
+  
+      console.log("Contract call success", data);
+    } catch (error) {
+      console.log("Contract call failure", error);
+    }
+  };
+  
+  
+  
+  
+
+  const getCampaigns = async () => {
+    const campaigns = await contract.call('getCampaigns');
+
+    const parsedCampaings = campaigns.map((campaign, i) => ({
+      owner: campaign.owner,
+      title: campaign.title,
+      description: campaign.description,
+      target: ethers.utils.formatEther(campaign.target.toString()),
+      deadline: campaign.deadline.toNumber(),
+      amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+      image: campaign.image,
+      pId: i
+    }));
+
+    return parsedCampaings;
+  }
+
+  const getUserCampaigns = async () => {
+    const allCampaigns = await getCampaigns();
+
+    const filteredCampaigns = allCampaigns.filter((campaign) => campaign.owner === address);
+
+    return filteredCampaigns;
+  }
+
+  const donate = async (pId, amount) => {
+    const data = await contract.call('donateToCampaign', pId, { value: ethers.utils.parseEther(amount)});
+
+    return data;
+  }
+
+  const getDonations = async (pId) => {
+    const donations = await contract.call('getDonators', pId);
+    const numberOfDonations = donations[0].length;
+
+    const parsedDonations = [];
+
+    for(let i = 0; i < numberOfDonations; i++) {
+      parsedDonations.push({
+        donator: donations[0][i],
+        donation: ethers.utils.formatEther(donations[1][i].toString())
+      })
+    }
+
+    return parsedDonations;
+  }
+
+
+  return (
+    <StateContext.Provider
+      value={{ 
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+        getUserCampaigns,
+        donate,
+        getDonations
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(StateContext);
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import "./styles/globals.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+import { Sepolia } from "@thirdweb-dev/chains";
+import { StateContextProvider } from './context';
+import App from './App';
+import './index.css';
 
-// This is the chain your dApp will work on.
-// Change this to the chain your app is built for.
-// You can also import additional chains from `@thirdweb-dev/chains` and pass them directly.
-const activeChain = "ethereum";
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const container = document.getElementById("root");
-const root = createRoot(container);
 root.render(
-  <React.StrictMode>
-    <ThirdwebProvider activeChain={activeChain}>
-      <App />
-    </ThirdwebProvider>
-  </React.StrictMode>
-);
+  <ThirdwebProvider activeChain={ Sepolia }> 
+    <Router>
+      <StateContextProvider>
+        <App />
+      </StateContextProvider>
+    </Router>
+  </ThirdwebProvider> 
+)
